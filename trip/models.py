92883ebfb1604789d5cc7301bbfc0cdeda45bf4a
Emmanuel-As <a.emmanuel0616@gmail.com>
2024-04-01 21:11:55 -0400

    feat: Setup database and image rendering

--- trip/models.py
+++ trip/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator
+
+User = get_user_model()
+
 
 # Create your models here.
+class Trip(models.Model):
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=2)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='trips')
+
+    def __str__(self):
+        return self.city
+
+
+class Note(models.Model):
+    EXCURSION = (
+        ("event", "Event"),
+        ("dining", "Dining"),
+        ("experience", "Experience"),
+        ("general", "General"),
+    )
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='notes')
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    type = models.CharField(max_length=100, choices=EXCURSION)
+    img = models.ImageField(upload_to="notes", blank=True, null=True)
+    rating = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(5)])
+
+    def __str__(self):
+        return f"{ self.name } in { self.trip.city }"
--- trip/urls.py
+++ trip/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 from django.urls import path
 from .views import *
 
+# For image rendering as well
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', HomeView.as_view(), name='home')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
