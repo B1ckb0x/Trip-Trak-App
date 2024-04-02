c898572e107dfea69506029c9d5560395aaa94c8
Emmanuel-As <a.emmanuel0616@gmail.com>
2024-04-01 21:16:47 -0400

    fix: Change urls pattern and import across project and config

--- trip/urls.py
+++ trip/urls.py
@@ -17,13 +17,6 @@ Including another URLconf
 from django.urls import path
 from .views import *
 
-# For image rendering as well
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path('', HomeView.as_view(), name='home')
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
