7c43daa9615ce1d9ab59ef9578ec7ed7b13d76af
Emmanuel-As <a.emmanuel0616@gmail.com>
2024-04-22 11:59:23 -0400

    final product

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Django ZTM\Trip-Trak-App\env
+set VIRTUAL_ENV=C:\Users\Asomu\Trip-Trak-App-1\env
 
 if not defined PROMPT set PROMPT=$P$G
 
